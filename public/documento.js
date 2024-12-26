c62ff8e9c357ee41235e520d2f7d3ffe43c9a43e
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-26 18:33:21 -0300

    Emitindo eventos

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,8 @@
 const socket = io();
+
+const textEditor = document.getElementById('editor-texto');
+
+//Observa sempre que alguem soltar uma tecla no editor de texto
+textEditor.addEventListener('keyup', () => {
+    socket.emit('texto_editor', textEditor.value);
+});
--- src/socket-back.js
+++ src/socket-back.js
@@ -3,4 +3,9 @@ import io from './server.js';
 
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou! ID: ' + socket.id);
+    
+    socket.on('texto_editor', (text) => {
+        console.log(text);
+       // socket.broadcast.emit('texto_editor', texto);
+    });
 });
