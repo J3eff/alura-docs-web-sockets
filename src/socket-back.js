4e453e2e4c7694a533ce5e41a7cff3a743574563
Jefferson Brandão <jeffersonvideo125@gmail.com>
2024-12-27 08:30:43 -0300

    Guardando os dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,27 @@
-
 import io from './server.js';
+
+const documentos = [
+    { 
+        nome: "JavaScript", 
+        texto: "texto de javascript"
+        
+    },
+    { 
+        nome: "Node", 
+        texto: "texto de node"
+    },
+    { 
+        nome: "Socket.io", 
+        texto: "texto de socket.io"
+    }
+]
 
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou! ID: ' + socket.id);
 
     socket.on('selecionar_documento', (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento);
+        console.log(documento);
         socket.join(nomeDocumento);
     });
 
@@ -12,3 +29,8 @@ io.on('connection', (socket) => {
         socket.to(nomeDocumento).emit('texto_editor_cliente', texto);
     });    
 });
+
+function encontrarDocumento(nomeDocumento) {
+    return documentos.find(doc => doc.nome === nomeDocumento);
+}
+
