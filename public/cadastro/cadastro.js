f8f9ff8720cb6b9ea497111cc4e77b904a7b259d
Jefferson Brandão <jeffersonvideo125@gmail.com>
2025-01-02 17:36:46 -0300

    feat: Enviando dados de cadastro

---
+++ public/cadastro/cadastro.js
@@ -0,0 +1,12 @@
+import { emitirCadastrarUsuario } from "./socket-front-cadastro.js";
+
+const form = document.getElementById("form-cadastro");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const usuario = form["input-usuario"].value;
+    const senha = form["input-senha"].value;
+
+    emitirCadastrarUsuario({ usuario, senha });
+})
